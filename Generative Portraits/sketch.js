7e989c387722c6e458a4a3b47ff0638e82882d46
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-03-31 14:13:37 -0400

    Manually changing colors

--- Generative Portraits/sketch.js
+++ Generative Portraits/sketch.js
@@ -13,10 +13,15 @@ function draw() {
   for (y = 0; y < height; y++) {
     for (x = 0; x < width; x++) {
       index = (x+y*width)*4
-      pixels[index + 0] = frog.pixels[index + 0]
-      pixels[index + 1] = frog.pixels[index + 1]
-      pixels[index + 2] = frog.pixels[index + 2]
+      pixels[index + 0] = 255  //frog.pixels[index + 0]
+      pixels[index + 1] = 255 - frog.pixels[index + 1]
+      pixels[index + 2] = 255 - frog.pixels[index + 2]
       pixels[index + 3] = frog.pixels[index + 3]
+
+      if (x % 100 == 0 && y % 100 == 0) {
+        fill(255,0,0)
+        circle(x,y,100)
+      }
     }
   }
   updatePixels();
