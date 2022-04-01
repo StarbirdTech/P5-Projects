580f0c980576ea9ec046a4370968fd6ecb9c5c2e
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-04-01 09:17:54 -0400

    Adding draw modes

--- Generative Portraits/sketch.js
+++ Generative Portraits/sketch.js
@@ -7,6 +7,13 @@ let mode = 0
 function preload()
 {
   frog = loadImage("curri.jpg")
+  //invert image colors
+  frog.loadPixels()
+  for (let i = 0; i < frog.pixels.length; i += 4) {
+    frog.pixels[i] = 255 - frog.pixels[i]
+    frog.pixels[i + 1] = 255 - frog.pixels[i + 1]
+    frog.pixels[i + 2] = 255 - frog.pixels[i + 2]
+  }
 }
 
 function setup() {
@@ -23,9 +30,11 @@ function draw() {
   loadPixels();
   frog.loadPixels();
   frameRate(120)
+  drawCircles()
+  /*
   for(i = 0 ;i<30;i++)
       paintWithPixels()
-  
+  */
 }
 
 
@@ -71,17 +80,84 @@ function blur()
   }
 }
 
-/*
-function mousePressed()
-{
-  save()
-}
-*/
-
 function keyPressed() {
   mode ++
 
   if (keyCode == 28) {
     mode = 0
   }
+}
+
+// save the canvas as a png
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas('sketch', 'png');
+  }
+}
+
+// randomly draw circles using the color data from the image
+function drawCircles() {
+  for (let i = 0; i < 1000; i++) {
+    let x = random(width);
+    let y = random(height);
+    let d = random(1, 10);
+    noStroke();
+    fill(frog.get(x, y));
+    ellipse(x, y, d, d);
+  }
+}
+
+// randomly draw lines using the color data from the image
+function drawLines() {
+  for (let i = 0; i < 1000; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+    noStroke();
+    fill(frog.get(x1, y1));
+    ellipse(x1, y1, 10, 10);
+    fill(frog.get(x2, y2));
+    ellipse(x2, y2, 10, 10);
+    stroke(frog.get(x1, y1));
+    line(x1, y1, x2, y2);
+  }
+}
+
+function wanderingLine() {
+  //limit line length to 10 px
+  let maxLength = 10;
+  //start the line at a random position
+  let x = random(width);
+  let y = random(height);
+  //start the line at a random angle
+  let a = random(TWO_PI);
+  //start the line at a random length
+  let len = random(maxLength);
+  //draw the line
+  stroke(frog.get(x, y));
+  line(x, y, x + len * cos(a), y + len * sin(a));
+}
+/*
+  for (let i = 0; i < 1000; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+    noStroke();
+    fill(frog.get(x1, y1));
+    ellipse(x1, y1, 10, 10);
+    fill(frog.get(x2, y2));
+    ellipse(x2, y2, 10, 10);
+    stroke(frog.get(x1, y1));
+    line(x1, y1, x2, y2);
+  }
+}*/
+
+// function to crop and resize the image to fill the canavs
+function resize() {
+  //crop the image to the size of the canvas
+  image(frog, 0, 0, width, height);
+  //resize the image to the size of the canvas
+  frog.resize(width, height);
 }
