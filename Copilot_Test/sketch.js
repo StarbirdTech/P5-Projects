982898156ce9220d8ee72758d954be351826bbd3
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-04-01 13:05:57 -0400

    Copilot expiriments

--- Copilot_Test/sketch.js
+++ Copilot_Test/sketch.js
@@ -1,3 +1,27 @@
+// make grid of squares the size of the window with random colors
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+  for (var i = 0; i < width; i += 10) {
+    for (var j = 0; j < height; j += 10) {
+      fill(random(255), random(255), random(255));
+      rect(i, j, 10, 10);
+    }
+  }
+}
+
+//create button to refresh page
+function draw() {
+  if (mouseIsPressed) {
+    background(0);
+    noStroke();
+    for (var i = 0; i < width; i += 10) {
+      for (var j = 0; j < height; j += 10) {
+        fill(random(255), random(255), random(255));
+        rect(i, j, 10, 10);
+      }
+    }
+  }
 }
